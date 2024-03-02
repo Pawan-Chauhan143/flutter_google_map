4d197714af74c63fecbb9f5b1403ebf47dd5113f
Pawan Kumar Chauhan <pawan.chauhan15072003@gmail.com>
2024-03-02 22:36:56 +0530

    // new work on google maps

--- lib/user_current_location.dart
+++ lib/user_current_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class GetUserCurrentLocation extends StatefulWidget {
   const GetUserCurrentLocation({super.key});
@@ -10,6 +11,8 @@ class GetUserCurrentLocation extends StatefulWidget {
 class _GetUserCurrentLocationState extends State<GetUserCurrentLocation> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
